32dc56877210e502b436e798f60669fd07ba6c62
jahboom1 <nomyweather@gmail.com>
2026-07-20 12:36:36 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,38 +1,21 @@
 #!/bin/sh
-
-# Создаём конфиг
-cat > /usr/local/etc/xray/config.json << EOF
+mkdir -p /etc/xray
+cat > /etc/xray/config.json << EOF
 {
-  "log": {
-    "loglevel": "warning"
-  },
-  "inbounds": [
-    {
-      "port": $PORT,
-      "protocol": "vless",
-      "settings": {
-        "clients": [
-          {
-            "id": "$UUID"
-          }
-        ],
-        "decryption": "none"
-      },
-      "streamSettings": {
-        "network": "ws",
-        "wsSettings": {
-          "path": "$WSPATH"
-        }
-      }
+  "log": {"loglevel": "warning"},
+  "inbounds": [{
+    "port": $PORT,
+    "protocol": "vless",
+    "settings": {
+      "clients": [{"id": "$UUID"}],
+      "decryption": "none"
+    },
+    "streamSettings": {
+      "network": "ws",
+      "wsSettings": {"path": "$WSPATH"}
     }
-  ],
-  "outbounds": [
-    {
-      "protocol": "freedom"
-    }
-  ]
+  }],
+  "outbounds": [{"protocol": "freedom"}]
 }
 EOF
-
-# Запускаем Xray
-xray run
+xray run -c /etc/xray/config.json
